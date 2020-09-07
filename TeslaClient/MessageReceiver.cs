426e30cfd0a5cc6959ecb17a2306233dc4de57b7
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 15:55:27 +0300

    fields and ctors added to MessageReceiver

--- TeslaClient/MessageReceiver.cs
+++ TeslaClient/MessageReceiver.cs
@@ -1,7 +1,17 @@
-﻿namespace TeslaClient
+﻿using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TeslaClient
 {
     public class MessageReceiver
     {
-
+        private NetworkStream _nwStream;
+        private IFormatter _binaryFormatter;
+        public MessageReceiver(NetworkStream networkStream)
+        {
+            _nwStream = networkStream;
+            _binaryFormatter = new BinaryFormatter();
+        }
     }
 }
