85bab2ae4dff9857f6b0b3d11e4af5e98fb27f95
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:11:05 +0300

    TeslaServer with props & ctor added

---
+++ TeslaServer/TeslaServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TeslaServer
+{
+    public class TeslaServer
+    {
+        private IPAddress _localAddress;
+        private TcpListener _server;
+        private ConcurrentDictionary<string, TcpClient> _clientsList;
+
+        public TeslaServer(int port)
+        {
+            _localAddress = IPAddress.Parse("127.0.0.1");
+            _server = new TcpListener(_localAddress, port);
+            _clientsList = new ConcurrentDictionary<string, TcpClient>();
+        }
+    }
+}
