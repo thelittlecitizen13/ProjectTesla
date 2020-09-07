db5637c64050d19b71ce713d395df525b1c2c9dd
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 17:50:59 +0300

    IsSendPicture validation added

--- TeslaClient/InputManager.cs
+++ TeslaClient/InputManager.cs
@@ -1,7 +1,24 @@
-﻿namespace TeslaClient
+﻿using System;
+namespace TeslaClient
 {
     public class InputManager
     {
-
+        public string GetUserInput(string textToDisplay)
+        {
+            Console.WriteLine(textToDisplay);
+            return Console.ReadLine();
+        }
+        public bool IsSendPicture(string input)
+        {
+            return input.ToLower() == "picture" || input.ToLower().StartsWith("picture;");
+        }
+        public bool IsSendScreenShot(string input)
+        {
+            return input.ToLower() == "picture";
+        }
+        public bool IsSendLocalPicture(string input)
+        {
+            return input.ToLower().StartsWith("picture;");
+        }
     }
 }
