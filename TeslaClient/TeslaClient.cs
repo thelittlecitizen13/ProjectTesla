5c0449e33bcf22195e015fc1b62da1cdec18cebb
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:16:00 +0300

    TeslaClient Project and class added

---
+++ TeslaClient/TeslaClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TeslaClient
+{
+    public class TeslaClient
+    {
+        private IPAddress _serverAddress;
+        private int _port;
+        private TcpClient _client;
+        public string Name;
+
+        public TeslaClient(string address, int port, int clientNumber)
+        {
+            _serverAddress = IPAddress.Parse(address);
+            _port = port;
+            Name = "client" + clientNumber;
+            _client = new TcpClient(_serverAddress.ToString(), _port);
+        }
+    }
+}
