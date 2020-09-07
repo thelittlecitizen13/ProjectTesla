1f95a193d00c07d6447bde925cf74ec840adeeda
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 15:42:15 +0300

    TakeScreenShot method implemented

--- TeslaClient/MessageSender.cs
+++ TeslaClient/MessageSender.cs
@@ -1,22 +1,30 @@
 ﻿using System.Drawing;
 using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using TeslaCommon;
 
 namespace TeslaClient
 {
     public class MessageSender
     {
         private NetworkStream nwStream;
+        private IFormatter _binaryFormatter;
         public MessageSender(NetworkStream networkStream)
         {
             nwStream = networkStream;
+            _binaryFormatter = new BinaryFormatter();
         }
         public void SendNewMessage()
         {
 
         }
-        public void SendNewTextMessage(string Text)
+        public void SendNewTextMessage(string text, ClientData src, ClientData dst)
         {
+            // ToDo: change the SRC and DST to real 
+            TextMessage message = new TextMessage(text, src, dst);
+
             byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(Text);
             nwStream.Write(bytesToSend, 0, bytesToSend.Length);
         }
@@ -24,5 +32,6 @@ namespace TeslaClient
         {
 
         }
+        
     }
 }
