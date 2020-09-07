fa1ef5298d9d8c11e181de4a37feb0b8291c01a4
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:13:01 +0300

    client Registration at server implemented

--- TeslaServer/TeslaServer.cs
+++ TeslaServer/TeslaServer.cs
@@ -19,5 +19,60 @@ namespace TeslaServer
             _server = new TcpListener(_localAddress, port);
             _clientsList = new ConcurrentDictionary<string, TcpClient>();
         }
+        private bool registerClient(TcpClient client)
+        {
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string clientName = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            if (tryAddClientToList(clientName, client))
+            {
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes($"Welcome, {clientName}");
+                connectionEstablishedPrint(client, clientName);
+                //---send the text---
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                SendToAllClients($"{clientName} joined!");
+                return true;
+            }
+            else
+            {
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes($"{clientName} name is already taken");
+                //---send the text---
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                return false;
+            }
+
+        }
+        private bool tryAddClientToList(string name, TcpClient client)
+        {
+            if (!_clientsList.ContainsKey(name))
+            {
+                _clientsList.TryAdd(name, client);
+                return true;
+            }
+            return false;
+
+        }
+        private void removeClientFromList(TcpClient client)
+        {
+
+            string clientName = "";
+            foreach (var clt in _clientsList)
+            {
+                if (clt.Value == client)
+                {
+                    clientName = clt.Key;
+                    _clientsList.TryRemove(clientName, out client);
+                    Console.WriteLine($"{clientName} left the chat!");
+                    break;
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(clientName))
+                SendToAllClients($"{clientName} left!");
+        }
     }
 }
