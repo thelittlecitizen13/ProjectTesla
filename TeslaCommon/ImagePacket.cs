9f0c48a8de5c7178e7d115a30949b6534f19502c
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:52:35 +0300

    ImagePacket class implemented

---
+++ TeslaCommon/ImagePacket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace TeslaCommon
+{
+    public class ImagePacket : IPacket
+    {
+        public ClientData Source { get; set; }
+        public ClientData Destination { get; set; }
+        public DateTime MessageTime { get; set; }
+        public Bitmap Image { get; set; }
+        public ImagePacket(Bitmap img, ClientData src, ClientData dst)
+        {
+            Source = src;
+            Destination = dst;
+            Image = img;
+            MessageTime = DateTime.Now;
+        }
+    }
+}
