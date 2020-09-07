67cafd84bebd867616b3b0c47cdb5c68c4dccf6e
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 16:29:27 +0300

    DisplayAnImage method implemented

--- TeslaClient/OutputManager.cs
+++ TeslaClient/OutputManager.cs
@@ -1,7 +1,23 @@
-﻿namespace TeslaClient
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+
+
+namespace TeslaClient
 {
     public class OutputManager
     {
-
+        public void DisplayAnImage(string imgPath)
+        {
+            try
+            {
+                Process.Start("cmd", $"/c {imgPath}");
+            }
+            catch
+            {
+                System.Console.WriteLine("Cannot open image");
+            }
+            
+        }
     }
 }
