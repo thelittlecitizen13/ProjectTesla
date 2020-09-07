93789643d0d971c97d82f248aef38bce9e6ba989
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 15:12:43 +0300

    ServerConnector class added to TeslaClient

--- TeslaClient/MessageSender.cs
+++ TeslaClient/MessageSender.cs
@@ -1,7 +1,17 @@
-﻿namespace TeslaClient
+﻿using System.Net.Sockets;
+
+namespace TeslaClient
 {
     public class MessageSender
     {
+        private NetworkStream nwStream;
+        public MessageSender(NetworkStream networkStream)
+        {
+            nwStream = networkStream;
+        }
+        public void SendNewMessage()
+        {
 
+        }
     }
 }
