2884d4a2f393f114b3895cff3556996f0b27f7ca
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:44:47 +0300

    ClientData class added at TeslaCommon

---
+++ TeslaCommon/ClientData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeslaCommon
+{
+    public class ClientData
+    {
+        public string UID { get; set; }
+        public string ClientName { get; set; }
+        public ClientData(string clientName)
+        {
+            ClientName = clientName;
+            UID = Guid.NewGuid().ToString();
+        }
+    }
+}
