581fab386117fb230cda8bc40e08d0527d690103
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 16:34:43 +0300

    Save image implemented

--- TeslaClient/OutputManager.cs
+++ TeslaClient/OutputManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 
 
@@ -7,6 +9,11 @@ namespace TeslaClient
 {
     public class OutputManager
     {
+        private string _clientName;
+        public OutputManager(string clientName)
+        {
+            _clientName = clientName;
+        }
         public void DisplayAnImage(string imgPath)
         {
             try
@@ -18,6 +25,18 @@ namespace TeslaClient
                 System.Console.WriteLine("Cannot open image");
             }
             
+        }
+        public string SaveAnImage(Bitmap img)
+        {
+            string imgPath = CreateImageFolder();
+            img.Save(imgPath + "clientPrintScreen" + Guid.NewGuid() + ".jpg", ImageFormat.Jpeg);
+            return imgPath;
+        }
+        public string CreateImageFolder()
+        {
+            string path = @"C:\images\" + _clientName;
+            Directory.CreateDirectory(path);
+            return path;
         }
     }
 }
