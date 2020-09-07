79546cc48aaff56978594bd440f39b9d9147e01d
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:49:33 +0300

    TextPacket class implemented

---
+++ TeslaCommon/TextPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TeslaCommon
+{
+    public class TextPacket : IPacket
+    {
+        public ClientData Source { get; set; }
+        public ClientData Destination { get; set; }
+        public DateTime MessageTime { get; set; }
+        public string Message { get; set; }
+        public TextPacket(string msg, ClientData src, ClientData dst)
+        {
+            Source = src;
+            Destination = dst;
+            Message = msg;
+            MessageTime = DateTime.Now;
+        }
+    }
+}
