de6b70880afc3f187a93784979aef0da69cc6e63
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-07 14:18:16 +0300

    Write and Receive message functions added for TeslaClient

--- TeslaClient/TeslaClient.cs
+++ TeslaClient/TeslaClient.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace TeslaClient
 {
@@ -16,6 +18,33 @@ namespace TeslaClient
             _port = port;
             Name = "client" + clientNumber;
             _client = new TcpClient(_serverAddress.ToString(), _port);
+        }
+
+        private void WriteAMessage(NetworkStream nwStream)
+        {
+            Console.WriteLine("Your message:");
+            string msg = Console.ReadLine();
+            string textToSend = $"{Name}: {msg}";
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+            //---send the text---
+            Console.WriteLine("Sending : " + msg);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+        private void ReceiveMessages(NetworkStream nwStream)
+        {
+            //---read back the text---
+            while (true)
+            {
+                byte[] bytesToRead = new byte[_client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, _client.ReceiveBufferSize);
+                string received = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                if (string.IsNullOrWhiteSpace(received))
+                {
+                    break;
+                }
+                Console.WriteLine("Received : " + received);
+            }
         }
     }
 }
