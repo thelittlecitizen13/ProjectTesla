fdfcad10a01866005cfcd910b61f19d4652dce54
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-08 11:17:46 +0300

    IMember interface added

---
+++ TeslaCommon/IMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeslaCommon
+{
+    interface IMember
+    {
+        string Name { get; set; }
+        MemberData Data { get; set; }
+    }
+}
