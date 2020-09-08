5825eebf9a8ae2e4394e187662169d48176bd0fd
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-08 11:21:43 +0300

    User class implemented

---
+++ TeslaCommon/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace TeslaCommon
+{
+    public class User : IMember
+    {
+        public string Name { get; set; }
+        public MemberData Data { get; set; }
+        public TcpClient client { get; set; }
+        public NetworkStream nwStream { get; set; }
+        public User(MemberData memberData, TcpClient tcpClient)
+        {
+            Name = memberData.MemberName;
+            Data = memberData;
+            client = tcpClient;
+            nwStream = client.GetStream();
+        }
+    }
+}
