c6e36713288c7562f1559fe072866b8fdba082bd
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-09 10:04:45 +0300

    GroupMessage class added

---
+++ TeslaCommon/GroupMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeslaCommon
+{
+    [Serializable]
+    public class GroupMessage : IMessage
+    {
+        public IMemberData Source { get; set; }
+        public IMemberData Destination { get; set; }
+        public UserData Author { get; set; }
+        public DateTime MessageTime { get; set; }
+        public string Message { get; set; }
+        public GroupMessage(string msg, UserData author, GroupData src, GroupData dst)
+        {
+            Source = src;
+            Destination = dst;
+            Author = author;
+            Message = msg;
+            MessageTime = DateTime.Now;
+        }
+    }
+}
