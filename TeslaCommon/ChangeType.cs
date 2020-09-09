5e58caa93f0b875ea9fdfdc7045f2891b954f84d
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-09 10:00:22 +0300

    ChangeType enum and GroupUpdateMessage created

---
+++ TeslaCommon/ChangeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeslaCommon
+{
+    public enum ChangeType
+    {
+        Created,
+        Updated,
+        Deleted
+    }
+}
---
+++ TeslaCommon/GroupMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeslaCommon
+{
+    [Serializable]
+    public class GroupMessage : IMessage
+    {
+        public IMemberData Source { get; set; }
+        public IMemberData Destination { get; set; }
+        public DateTime MessageTime { get; set; }
+        public GroupData GroupChanged { get; set; }
+        public ChangeType typeOfChange { get; set; }
+        public GroupMessage(GroupData group, ChangeType action, UserData src, UserData dst)
+        {
+            GroupChanged = group;
+            typeOfChange = action;
+            Source = src;
+            Destination = dst;
+            MessageTime = DateTime.Now;
+        }
+    }
+}
